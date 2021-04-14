3195d5bddc3431b9bee680582e9d7fe4731594da
Roy <rgonzalez@tradestation.com>
2021-04-14 13:48:21 -0600

    Adding Grafana

--- run.sh
+++ run.sh
@@ -5,6 +5,13 @@ helm repo update
 helm install nginx nginx/ingress-nginx --set controller.publishService.enabled=true
 
 #install cert-manager
+helm repo add jetstack https://charts.jetstack.io
+helm repo update
+helm install cert-manager jetstack/cert-manager --namespace cert-manager --version v1.2.0 --set installCRDs=true
+
+
+
+
 kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v0.16.1/cert-manager.yaml
 
 #create the issuer
@@ -15,7 +22,7 @@ kubectl apply -f issuer-prod.yaml
 #grafana
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
-helm install loki grafana/loki  -f ./grafana-values.yaml
+helm install grafana grafana/grafana  -f ./grafana-values.yaml
 
 #prometheus
 helm repo add prometheus https://prometheus-community.github.io/helm-charts
@@ -24,9 +31,12 @@ helm install prometheus prometheus/prometheus  -f ./prometheus-values.yaml
 
 
 kubectl create secret generic logzio-secret \
---from-literal=logzio-token='$LOGZ_TOKEN' \
+--from-literal=logzio-token=$LOGZ_TOKEN \
 --from-literal=logzio-listener='https://listener.logz.io:8071' \
 -n default
 
 kubectl create -f logconfig.yaml
 kubectl create -f logzio-daemonset.yaml
+
+#metrics api
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
